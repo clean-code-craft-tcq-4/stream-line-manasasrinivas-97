35dd80bdd459a211ef21996aefc05b340a5fa9d6
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-10-13 20:30:29 +0530

    Update sensorTemperature.cpp

--- sensorTemperature.cpp
+++ sensorTemperature.cpp
@@ -1,1 +1,13 @@
+#include "sensorTemperature.h"
+
+vector<int> generateTemperatureValues(int minTemperatureValue, int maxTemperatureValue, int numberOfReadings)
+{
+    vector<int> tempValueList;
+    for(int count = 0; count < numberOfReadings; count++)
+    {
+        int rValue = rand() % maxTemperatureValue + minTemperatureValue;
+        temperatureValueList.push_back(rValue);
+    }
+    return tempValueList;
+}
 

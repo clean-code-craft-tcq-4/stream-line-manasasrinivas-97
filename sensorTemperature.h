4215eec3a88625abf01a0c789618522af2348994
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-10-13 20:25:50 +0530

    Update and rename sensorTemperatureStatus.h to sensorTemperature.h

---
+++ sensorTemperature.h
@@ -0,0 +1,14 @@
+#ifndef SENSORTEMPERATURE_H
+#define SENSORTEMPERATURE_H
+
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+
+const int MAX_TEMPERATURE_VALUE 100
+const int MIN_TEMPERATURE_VALUE -50
+
+using namespace std;
+
+vector<int> generateTemperatureValues(int minTemperatureValue, int maxTemperatureValue, int numberOfReadings);
+#endif

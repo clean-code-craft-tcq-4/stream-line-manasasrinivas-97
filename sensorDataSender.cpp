6c8c1809ee209714ec5ad9f5762653dafd9ea463
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-10-13 20:42:56 +0530

    Update sensorDataSender.cpp

--- sensorDataSender.cpp
+++ sensorDataSender.cpp
@@ -1,5 +1,21 @@
+#include "sensorDataSender.h"
 
+bool transferData()
+{
+    bool sendStatus = false;
+    vector<int> temperatureData = generateTemperatureValues(MAX_TEMPERATURE_VALUE , MIN_TEMPERATURE_VALUE , NUMBER_OF_SAMPLES);
+    vector<int> chargeData = generateChargeValues(MAX_CHARGE_VALUE , MIN_CHARGE_VALUE , NUMBER_OF_SAMPLES);
+    if((temperatureData.size() == NUMBER_OF_SAMPLES) && (chargeData.size() == NUMBER_OF_SAMPLES))
+    {
+        for(int count = 0; count < NUMBER_OF_SAMPLES; count ++)
+        {
+            cout << temperatureData[count]<< "," << chargeData[count] <<endl;
+        }
+        sendStatus = true;
+    }
+    return sendStatus;
 
+}
 
 
 
